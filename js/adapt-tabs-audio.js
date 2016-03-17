3f9825796c5d3d826aebe32a9011f5a77d1a320a
Dan Gray <dan@sinensis.co.uk>
2016-03-17 14:33:26 +0000

    Add config checks

--- js/adapt-tabs-audio.js
+++ js/adapt-tabs-audio.js
@@ -21,7 +21,7 @@ define(function(require) {
 			this.showContentItemAtIndex(0, true);
 			this.setTabSelectedAtIndex(0);
 
-			if (this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled && Adapt.config.get('_reducedText')._isEnabled) {
+			if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isReducedTextEnabled && this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
 				this.replaceText(Adapt.audio.textSize);
             }
 		},
@@ -70,7 +70,7 @@ define(function(require) {
             var currentItem = this.getCurrentItem($item.index());
 
 			///// Audio /////
-            if (this.model.has('_audio') && this.model.get('_audio')._isEnabled && Adapt.audio.audioClip[this.model.get('_audio')._channel].status==1) {
+            if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isEnabled && this.model.has('_audio') && this.model.get('_audio')._isEnabled && Adapt.audio.audioClip[this.model.get('_audio')._channel].status==1) {
                 // Trigger audio
                 Adapt.trigger('audio:playAudio', currentItem._audio.src, this.model.get('_id'), this.model.get('_audio')._channel);
             }
@@ -137,7 +137,7 @@ define(function(require) {
 		// Reduced text
         replaceText: function(value) {
             // If enabled
-            if (this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled && Adapt.config.get('_reducedText')._isEnabled) {
+            if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isReducedTextEnabled && this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
                 // Change component title and body
                 if(value == 0) {
                     this.$('.component-title-inner').html(this.model.get('displayTitle')).a11y_text();
